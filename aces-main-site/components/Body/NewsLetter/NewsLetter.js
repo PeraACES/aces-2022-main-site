c99b3942428a363d962cf2c5bbe4d2aa8a37afc4
AmindaAmarasinghe <73498089+AmindaAmarasinghe@users.noreply.github.com>
2022-06-06 12:37:39 +0530

    2019 News Letter

--- aces-main-site/components/Body/NewsLetter/NewsLetter.js
+++ aces-main-site/components/Body/NewsLetter/NewsLetter.js
@@ -6,11 +6,15 @@ class NewsLetter extends Component {
     render() {
         return (
             <div>
-                <h4 className={styles.head}>NEWSLETTERS</h4>
+                <h4 className={styles.head}>NEWS LETTERS</h4>
                 <div className="row p-1 mx-1 mx-md-5">
-                    <div className='col-12 col-md-4'><Card link="#" image="img/bg_1.jpg" desc="News Letter 1" /></div>
-                    <div className='col-12 col-md-4'><Card link="#" image="img/bg_1.jpg" desc="News Letter 2" /></div>
-                    <div className='col-12 col-md-4'><Card link="#" image="img/bg_1.jpg" desc="News Letter 3" /></div>
+                    <div className='col-12 col-md-4'>
+                        {/* <Card link="#" image="img/bg_1.jpg" desc="News Letter 1" /> */}
+                    </div>
+                    <div className='col-12 col-md-4'><Card link="https://viewer.joomag.com/aces-outline-the-newsletter-2019/0669634001576069380?short" image="img/bg_1.jpg" desc="News Letter 2019" /></div>
+                    <div className='col-12 col-md-4'>
+                        {/* <Card link="#" image="img/bg_1.jpg" desc="News Letter 3" /> */}
+                    </div>
                 </div>
             </div>
         )
